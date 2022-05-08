5d20932bfc9efdee4a68b24da770c3e35cf5b814
ashutosh <amahapatra13@gmail.com>
2022-05-08 11:51:56 +0530

    upload posts function

--- contracts/Diginotice.sol
+++ contracts/Diginotice.sol
@@ -4,4 +4,19 @@ pragma solidity ^0.8.10;
 contract Diginotice {
     string public name = "Diginotice";
     
+    //Store posts
+    mapping(uint => Post) public posts;
+
+    struct Post{
+        uint id;
+        string hash;
+        string description;
+        address author;
+    }
+    //Create posts
+    function uploadImage() public {
+        posts[1] = Post(1, 'abc123', 'Hello',address(0x0));
+    }
+    
+    //Comments
 }
