a6e643542f3330f868c4feae2d795940973f3e63
HP <avinashtiwary6300@gmail.com>
2022-05-08 15:25:10 +0530

    Basic FrontEnd Added

---
+++ front-end-diginotice/src/Components/Navbar1.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import logo from '../Images/logo.jpg'
+import { Navbar, NavbarBrand, Collapse, NavItem, NavLink, Nav, NavbarToggler } from 'reactstrap'
+import '../Css/Navbar1.css'
+
+
+const Navbar1 = (props) => {
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => setIsOpen(!isOpen);
+	return (
+		<div >
+			<Navbar color="warning" light-expand="md">
+				<NavbarBrand className="links" href="/">
+					<img src={logo} width="50" height="50" class="d-inline-block align-top " alt=""/>
+					DigiNotice
+				</NavbarBrand>
+					<NavbarToggler onClick={toggle} />
+					<Collapse isOpen={isOpen} navbar></Collapse>
+					<Nav >
+						<NavItem>
+						<NavLink className="links" href="/login">Login</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink className="links" href="/signup">Sign Up</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink className="links" href="/noticeboard">Notice Board</NavLink>
+						</NavItem>
+					</Nav >
+			</Navbar>
+		</div>
+
+
+	)
+}
+
+export default Navbar1;
