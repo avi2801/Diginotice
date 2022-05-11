7b10e849ce4b783a0bdc10d051e557b2c7e4ef65
Ashutosh Mahapatra <55703860+amahapatra13@users.noreply.github.com>
2022-05-11 18:41:46 +0530

    Revert "Added test cases."

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const { assert } = require('chai')
 const Diginotice = artifacts.require("../contracts/Diginotice")
 
 require('chai')
-    .use(require('chai-as-promised'))
+    .use(reqiure('chai-as-promised'))
     .should()
 
 contract('Diginotice', ([deployer, author, tipper]) => {
@@ -26,41 +26,5 @@ contract('Diginotice', ([deployer, author, tipper]) => {
             const name = await diginotice.name()
             assert.equal(name, 'Diginotice')
         })
-    })
-
-    describe('posts', async () => {
-        let result, postCount
-        const hash = 'abc123123123'
-    
-        before(async () => {
-          result = await diginotice.uploadImage(hash, 'Post description', { from: author })
-          postCount = await diginotice.postCount()
-        })
-
-        it('creates posts', async () => {
-            // SUCESS
-            assert.equal(postCount, 1)
-            const event = result.logs[0].args
-            assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
-            assert.equal(event.hash, hash, 'Hash is correct')
-            assert.equal(event.description, 'Post description', 'description is correct')
-            assert.equal(event.author, author, 'author is correct')
-      
-      
-            // FAILURE: Post must have hash
-            await diginotice.uploadImage('', 'Post description', { from: author }).should.be.rejected;
-      
-            // FAILURE: Post must have description
-            await diginotice.uploadImage('Post hash', '', { from: author }).should.be.rejected;
-        })
-        // from struct
-        
-        it('lists posts', async () => {
-            const post = await diginotice.posts(postCount)
-            assert.equal(post.id.toNumber(), postCount.toNumber(), 'id is correct')
-            assert.equal(post.hash, hash, 'Hash is correct')
-            assert.equal(post.description, 'Image description', 'description is correct')
-            assert.equal(post.author, author, 'author is correct')
-          })
     })
 })
